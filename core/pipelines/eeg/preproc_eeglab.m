e725f4aa3d82d62bf06cd9f37c96ae784e5ab669
basile <basile.pinsard@umontreal.ca>
2015-09-02 10:55:51 -0400

    wip

--- core/pipelines/eeg/preproc_eeglab.m
+++ core/pipelines/eeg/preproc_eeglab.m
@@ -2,6 +2,10 @@ function EEG=preproc_eeglab(path, filename, tr, outpath)
 
 [EEG,com] = pop_loadbv(path,filename);
 
+% compute a number of component depending on the length of recording
+%ncomp = round(log(EEG.pnts/EEG.srate/60)*2+3);
+ncomp = round((EEG.pnts/EEG.srate/60)/6+3)
+
 etype='R  1';
 Trigs=[];
 for E=1:length(EEG.event)
@@ -11,7 +15,7 @@ for E=1:length(EEG.event)
 end
 
 % add dummy triggers
-Trigs = [linspace(-3,-1,3)*(EEG.srate*tr)+Trigs(1) Trigs Trigs(end)+EEG.srate*tr];
+%Trigs = [linspace(-3,-1,3)*(EEG.srate*tr)+Trigs(1) Trigs Trigs(end)+EEG.srate*tr];
 
 ecg_chans = [];
 for i=1:EEG.nbchan
@@ -37,10 +41,13 @@ for i=1:EEG_fastr.nbchan
 end
 
 EEG_qrs = correct_qrs(EEG_qrs, qrs_events);
-EEG_pas = pop_fmrib_pas(EEG_qrs,'qrs_ECG','obs',5);
 
-pop_saveset(EEG_pas,'filename',[filename(1:end-4) '_gca_pas.set'], ...
-            'filepath',outpath)
+EEG_downsample = pop_resample(EEG_qrs, 250);
 
-    
-    
+pop_writebva(EEG_downsample,[outpath '/' filename(1:end-5) '_gca.vhdr']);
+
+EEG_pas = pop_fmrib_pas(EEG_downsample,'qrs_ECG','obs',ncomp);
+
+EEG_filt = pop_eegfiltnew(EEG_qrs, .5, 35);
+
+pop_writebva(EEG_pas,[outpath '/' filename(1:end-5) '_gca_pas_filt.vhdr']);
